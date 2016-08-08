02980dffed3b3b9e03332bdb548e4b2244777119
JaeMin-Choi <jaemin.jaemni@gmail.com>
2016-08-09 08:29:54 +0900

    stacked autoencoder ver

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 #include <fstream>
 #include "Layer.h"
 #include "SimpleAutoEncoder.h"
-
+#include "StackedAutoEncoder.h"
 using namespace std;
 
 #define MAX_EPOCH 1000000
 #define MAX_ERROR 0.001f
 
-#define INPUT_DATA_DIMENSION 3
-#define TRAINING_DATA_SIZE 8
+#define INPUT_DATA_DIMENSION 100
+#define TRAINING_DATA_SIZE 4
 
 #define LEARNING_RATE 0.05f
 
@@ -17,101 +17,115 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	FILE* myfile = fopen("result.txt", "a");
-	float train_data[8][INPUT_DATA_DIMENSION] = {
-		{ 0,0,0},
-		{0,0,1},
-		{0,1,0},
-		{0,1,1},
-		{1,0,0},
-		{1,0,1},
-		{1,1,0},
-		{1,1,1}
+	float training_data[TRAINING_DATA_SIZE][INPUT_DATA_DIMENSION] = {
+		{ 1,1,0,0,0,0,1,1,0,0,
+		0,1,0,0,0,0,0,0,1,0,
+		0,0,1,0,0,1,0,0,0,0,
+		0,1,0,0,1,1,1,0,1,0,
+		0,1,0,0,1,0,0,1,0,0,
+		0,1,0,0,0,0,0,0,0,0,
+		0,1,0,0,0,1,0,1,0,1,
+		0,0,0,0,0,1,0,0,0,0,
+		0,0,0,0,0,0,1,0,0,0,
+		0,0,0,0,0,0,1,1,1,1 },
+
+		{ 1,1,0,0,0,0,1,1,0,0,
+		0,0,0,1,0,0,0,1,1,0,
+		0,0,1,0,0,1,0,0,0,0,
+		0,1,0,0,1,0,1,1,1,0,
+		1,0,0,0,1,0,0,1,0,0,
+		0,1,0,0,0,1,0,0,0,0,
+		1,1,0,0,0,1,0,1,0,1,
+		0,0,0,0,0,0,0,1,0,0,
+		0,1,0,0,0,0,1,0,0,0,
+		0,1,0,0,0,0,0,1,1,1 },
+
+		{ 1,0,0,1,0,0,1,1,0,0,
+		0,1,0,0,0,0,0,0,1,0,
+		0,0,1,0,0,1,0,0,1,0,
+		0,1,0,0,1,1,1,0,1,0,
+		1,0,0,0,1,0,0,1,0,0,
+		0,1,0,0,0,1,1,0,1,0,
+		0,1,0,1,0,0,0,1,0,1,
+		0,0,0,0,0,1,0,0,0,0,
+		0,1,0,1,0,0,1,0,0,0,
+		0,0,0,0,0,0,1,1,1,1 },
+
+		{ 0,1,0,0,1,0,1,0,1,0,
+		0,0,0,1,1,0,0,1,1,0,
+		0,0,1,0,0,1,0,0,0,0,
+		1,1,0,0,1,0,1,1,1,0,
+		1,0,1,0,1,0,0,1,0,0,
+		0,1,0,0,0,1,0,0,0,0,
+		1,1,1,0,0,1,0,1,0,1,
+		0,1,0,0,0,0,1,1,0,0,
+		0,0,1,0,1,0,0,0,1,0,
+		0,1,0,1,1,0,0,1,0,1 },
+
 	};
+	int num_AE = 3;
+	int eachEncoder_outDim[] = { 50,25,10 };
 
-	const int hidden_dim = 6;
-	SimpleAutoEncoder myAE(INPUT_DATA_DIMENSION, hidden_dim);
-
-	printf("\n\nStart Training Simple-Autoencoder \n");
-	fprintf(myfile, "\n\nStart Training Simple-Autoencoder \n");
-
-	for (int epoch = 0; epoch < MAX_EPOCH; epoch++) {
-		float error = 0.f;
-
-		for (int i = 0; i < TRAINING_DATA_SIZE; i++) {
-			myAE.Back_Propagate(train_data[i]);
-			error += myAE.Get_Decoding_Error();
-		}
-
-		error /= TRAINING_DATA_SIZE;
-
-		myAE.Weight_Update(LEARNING_RATE);
-
-		if ((epoch + 1) % 100 == 0) {
-			printf("epoch = %d, error = %f \n", epoch + 1, error);
-			fprintf(myfile, "epoch = %d, error = %f \n", epoch + 1, error);
-
-			for (int j = 0; j < TRAINING_DATA_SIZE; j++) {
-				myAE.Decoding(train_data[j]);
-				float *decode_result = myAE.Get_Decoding_Result();
+	StackedAutoEncoder myStackedAE(num_AE, INPUT_DATA_DIMENSION, eachEncoder_outDim);
 
 
+	printf("Start Training Stacked-AutoEncoder \n");
+	fprintf(myfile, "Start Training Stacked-AutoEncoder \n");
 
-				printf("test%d: ( ", j);
-				fprintf(myfile, "test%d: ( ", j);
-				for (int k = 0; k < INPUT_DATA_DIMENSION; k++) {
-					printf("%f ", train_data[j][k]);
-					fprintf(myfile, "%f ", train_data[j][k]);
-				}
+	
+	for (int idx = 0; idx < num_AE; idx++) {
+		int for_breaking = 1;
+		for (int epoch = 0; epoch < MAX_EPOCH && for_breaking == 1; epoch++) {
+			float error = 0.f;
+			for (int i = 0; i < TRAINING_DATA_SIZE; i++) {
+				myStackedAE.Back_Propagate(training_data[i], idx);
+				error += myStackedAE.Get_Reproduct_Error(idx);
+			}
 
-				printf(") --> ( ");
-				fprintf(myfile, ") --> ( ");
-				for (int k = 0; k < INPUT_DATA_DIMENSION; k++) {
-					printf("%f ", decode_result[k]);
-					fprintf(myfile, "%f ", decode_result[k]);
-				}
-				printf(")\n");
-				fprintf(myfile, ")\n");
+			error /= TRAINING_DATA_SIZE;
+			if (error < MAX_ERROR) {
+				for_breaking = 0;
+				break;
+			}
+			myStackedAE.Weight_Update(LEARNING_RATE, idx);
 
+
+
+			if ((epoch + 1) % 100 == 0) {
+				printf("%d-th autoencoder, epoch = %d, error = %f \n", idx + 1, epoch + 1, error);
+				fprintf(myfile, "%d-th autoencoder, epoch = %d, error = %f \n", idx + 1, epoch + 1, error);
 			}
 		}
-
-		if (error < MAX_ERROR)
-			break;
 	}
 
-	printf("Finish Training AutoEncoder  \n\n");
-	fprintf(myfile, "Finish Training AutoEncoder  \n\n");
 
-	printf("Test AutoEncoder\n");
-	fprintf(myfile, "Test AutoEncoder\n");
 
-	for (int j = 0; j < TRAINING_DATA_SIZE; j++) {
-		myAE.Decoding(train_data[j]);
-		float *encode_result = myAE.Get_Encoding_Result();
-		float *decode_result = myAE.Get_Decoding_Result();
+	fprintf(myfile, "Finish Training Stacked-AutoEncoder \n\n");
 
-		printf("test%d: ( ", j);
-		fprintf(myfile, "test%d: ( ", j);
-		for (int k = 0; k < INPUT_DATA_DIMENSION; k++) {
-			printf("%f ", train_data[j][k]);
-			fprintf(myfile,"%f ", train_data[j][k]);
+
+	printf("Testing Stacked-AutoEncoder\n");
+	fprintf(myfile, "Testing Stacked-AutoEncoder\n");
+
+	for (int number = 0; number < num_AE; number++) {
+		fprintf(myfile, "\n\n%d-th autoencoder testing \n", number + 1);
+		for (int j = 0; j < TRAINING_DATA_SIZE; j++) {
+			myStackedAE.Decoding(training_data[j], number);
+			float *encoded = myStackedAE.Get_Encoding_Result(number);
+			float *reprod = myStackedAE.Get_Decoding_Result(number);
+			float *pinput = myStackedAE.Get_Input(number);
+			if (number == 0) {
+				for (int k = 0; k < INPUT_DATA_DIMENSION; k++) {
+					fprintf(myfile, "\t(%f, %f)", pinput[k], reprod[k]);
+				}
+			}
+			else {
+				for (int k = 0; k < eachEncoder_outDim[number]; k++) {
+					fprintf(myfile, "\t(%f, %f)", pinput[k], reprod[k]);
+				}
+			}
+			fprintf(myfile, "\n\n");
 		}
-		printf(") --> ( ");
-		fprintf(myfile,") --> ( ");
-		for (int k = 0; k < hidden_dim; k++) {
-			printf("%f ", encode_result[k]);
-			fprintf(myfile,"%f ", encode_result[k]);
-		}
-		printf(") --> ( ");
-		fprintf(myfile,") --> ( ");
-		for (int k = 0; k < INPUT_DATA_DIMENSION; k++) {
-			printf("%f ", decode_result[k]);
-			fprintf(myfile,"%f ", decode_result[k]);
-		}
-		printf(")\n");
-		fprintf(myfile,")\n");
 	}
-
 	fclose(myfile);
 
 	return 0;
